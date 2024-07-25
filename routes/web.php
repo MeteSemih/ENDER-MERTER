f225989d151bc033328a5e4dd6f374cfc54ebd91
Mete <korganmete@hotmail.com>
2024-07-25 18:25:06 +0300

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/reklamarkasi', function () {
-    return view('reklamarkasi_details');
-})->name('reklamarkasi_details');
-Route::get('/koseyazilari', array(\App\Models\KoseYazisi::class, 'index'))->name('koseyazilari.index');
+
